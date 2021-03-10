d2b225638f5b1f4ca492488bdaa6466eed1dd19c
mzw86020424 <mzw86020424@gmail.com>
2021-03-11 07:51:18 +0900

    mapを使いcomponentにpropsを渡す

--- src/components/App.js
+++ src/components/App.js
@@ -3,22 +3,46 @@ import Language from './Language';
 
 class App extends React.Component {
   render() {
+
+    const languageList = [
+      {
+        name: 'HTML & CSS',
+        image: 'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/react/html.svg'
+      },
+      {
+        name: 'JavaScript',
+        image: 'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/react/es6.svg'
+      },
+      {
+        name: 'React',
+        image: 'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/react/react.svg'
+      },
+      {
+        name: 'Ruby',
+        image: 'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/react/ruby.svg'
+      },
+      {
+        name: 'Ruby on Rails',
+        image: 'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/react/rails.svg'
+      },
+      {
+        name: 'Python',
+        image: 'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/react/python.svg'
+      }
+    ];
+
     return (
       <div>
         <h1>言語一覧</h1>
         <div className='language'>
-          <Language
-            name='HTML & CSS'
-            image='https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/react/html.svg'
-          />
-          <Language
-            name='JavaScript'
-            image='https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/react/es6.svg'
-          />
-          <Language
-            name='React'
-            image='https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/react/react.svg'
-          />
+          {languageList.map((languageItem) => {
+            return (
+              <Language
+                name={languageItem.name}
+                image={languageItem.image}
+              />
+            )
+          })}
         </div>
       </div>
     );
